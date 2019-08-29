0dfb3a6938f644f15e91a0700f0f64f5cf662cf2
jpreister4 <54559118+jpreister4@users.noreply.github.com>
2019-08-29 11:14:06 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Name: Joshua Preister
+ * Date: 8/29/2019
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Joshua Preister!\n");
+
+  return 0;
+}
